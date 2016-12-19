82305b4c92d42029fb1c0956659a07b15a530ed0
su <su.myat.hlaing@frobom.com>
2016-12-19 15:06:10 +0630

    design file entry form

--- assets/js/filelist.js
+++ assets/js/filelist.js
@@ -20,23 +20,54 @@ var FileList = React.createClass({
 			});
 		});
 
-		$('#file-create').on('click',function(event) {
-			console.log("---file create handler---");
-			event.preventDefault();
-			var fileName = prompt("Please enter file name", "");
-			var url = '/project/in/'+projectName+'/file/new';
-			var posting = $.post(url,{fileName: fileName});
-			posting.done(function(data) {
-				if (data.fileName) {
-					console.log(data.fileName);
-					var filesUpdated = self.state.files.slice();
-					filesUpdated.push(data);
-					self.setState({
-						files: filesUpdated
-					}
-				);
+		function addDtFile() {
+			var valid = true;
+			var fileName = $('#fileID').val();
+			if (valid && fileName) {
+				console.log("---file create handler---");
+				var url = '/project/in/'+projectName+'/file/new';
+				var posting = $.post(url,{fileName: fileName});
+				posting.done(function(data) {
+					if (data.fileName) {
+						console.log(data.fileName);
+						var filesUpdated = self.state.files.slice();
+						filesUpdated.push(data);
+						self.setState({
+							files: filesUpdated
+						}
+					);
+				}
+			});
+			dialog.dialog( "close" );
+			return valid;
+		} else {
+			dialog.dialog( "close" );
+		}
+	}
+
+	var dialog = $( "#dialog-form" ).dialog({
+		autoOpen: false,
+		height: 250,
+		width: 300,
+		modal: true,
+		buttons: {
+			"Create": addDtFile,
+			Cancel: function() {
+				dialog.dialog( "close" );
 			}
-		});
+		},
+		close: function() {
+			form[ 0 ].reset();
+		}
+	});
+
+	var form = dialog.find( "form" ).on( "submit", function( event ) {
+		event.preventDefault();
+		addDtFile();
+	});
+
+	$( "#file-create" ).button().on( "click", function() {
+		dialog.dialog( "open" );
 	});
 },
 
